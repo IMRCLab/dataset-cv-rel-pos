749390b2de51d3f49f1245ef6eb34e4b634607ec
akmaralAW <moldagalieva@tu-berlin.de>
2023-03-20 14:30:27 +0100

    flat hierarchy,updated dataset.yaml

--- dataset_pack.py
+++ dataset_pack.py
@@ -5,61 +5,45 @@ import os
 import shutil
 import cv2
 
-def clean(dataset_yaml_in, output_folder):
-    robot_names = [str(f).split('/')[-1] for f in Path(dataset_yaml_in).parent.iterdir() if f.is_dir()]
-    output_folder = Path(output_folder)
-    if output_folder.exists() and output_folder.is_dir():
-        shutil.rmtree(output_folder)
-        os.mkdir(output_folder)
-
-    for robot in robot_names:
-        folder = output_folder / robot
-        os.mkdir(folder)
-    # dataset_yaml_out = {
-    #     'calibration': {},
-    #     'images': {},
-    # }
+def clean(dataset_yaml_in, dataset_yaml_out):
+    output_folder = Path(dataset_yaml_out).parent
+    os.mkdir(output_folder)    
+    result = {
+        'calibration': {},
+        'images': {},
+    }
     with open(dataset_yaml_in, 'r') as f:
         dataset = yaml.load(f, Loader=yaml.CSafeLoader)
 
-    # copy calibration information and make it unique, if not identical
-        # calib_name_dict = dict()
-        # for calib_name in dataset['calibration']:
-        #     calib_name_new = calib_name
-        #     if calib_name in dataset_yaml_out['calibration']:
-        #         if dataset['calibration'][calib_name] != dataset_yaml_out['calibration'][calib_name]:
-        #             # calibration already exists and is different -> rename
-        #             i = 1
-        #             while True:
-        #                 calib_name_new = calib_name + "_" + str(i)
-        #                 if calib_name_new not in dataset_yaml_out['calibration']:
-        #                     break
-        #                 i = i + 1
+    result['calibration'] = dataset['calibration']
+    for full_image_name in dataset['images']:
+        image_name = Path(full_image_name).name
+        image_name_new = image_name
+        if (output_folder / Path(image_name_new)).exists():
+            j = 1
+            while True:
+                image_name_new = image_name[:-4] + "_" + str(j) + image_name[-4:]
+                if image_name_new not in result['images']:
+                    break
+                j = j + 1
+        shutil.copy(str((Path(dataset_yaml_in).parent / full_image_name)),str(Path(output_folder) / image_name_new))
+        rel_image_name = str((Path(output_folder) / image_name_new).relative_to(Path(dataset_yaml_out).parent))
+        result['images'][rel_image_name] = dataset['images'][full_image_name]
 
-        #     dataset_yaml_out['calibration'][calib_name_new] = dataset['calibration'][calib_name]
-        #     calib_name_dict[calib_name] = calib_name_new
-    for image_name in dataset['images']:
-            # dataset_yaml_out['images'][image_name] = dataset['images'][image_name]
-            image = cv2.imread(str((Path(dataset_yaml_in).parent / image_name)))
-            if (output_folder / Path(image_name).parent).exists():
-                 cv2.imwrite(str(Path(output_folder) / image_name), image)
-            else:
-                 os.makedirs(output_folder / Path(image_name).parent)
-                 cv2.imwrite(str(Path(output_folder) / image_name), image)
             
-    # yaml.Dumper.ignore_aliases = lambda *args : True
-    # with open(Path(output_folder) / 'dataset.yaml', 'w') as outfile:
-    #         yaml.dump(dataset_yaml_out, outfile)
-    shutil.copy(dataset_yaml_in, output_folder / 'dataset.yaml')
+    yaml.Dumper.ignore_aliases = lambda *args : True
+    with open(Path(dataset_yaml_out), 'w') as outfile:
+            yaml.dump(result, outfile)
+    
 
 
 def main():
     parser = argparse.ArgumentParser(description='Clean synthetic dataset')
     parser.add_argument('dataset_yaml_in', help="Path to input file")
-    parser.add_argument('folder_out', help="Path to output folder")
+    parser.add_argument('dataset_yaml_out', help="Path to output file")
     args = parser.parse_args()
 
-    clean(args.dataset_yaml_in, args.folder_out)
+    clean(args.dataset_yaml_in, args.dataset_yaml_out)
 
 if __name__ == "__main__":
     main()
